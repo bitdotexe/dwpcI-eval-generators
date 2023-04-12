009b2623e7328c915ca03e9ad59423486f3d5a1f
Uriel Torres <72411670+bitdotexe@users.noreply.github.com>
2023-04-11 21:08:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
 const superHeroTeams = [
   {
-    name: 'Dream Team',
+    name: "Dream Team",
     heroes: [
       {
-        id: '1',
-        name: 'Superman',
-        superPowers: ['speed', 'x-ray vision', 'flying'],
+        id: "1",
+        name: "Superman",
+        superPowers: ["speed", "x-ray vision", "flying"],
       },
       {
-        id: '2',
-        name: 'Spieder-Man',
-        superPowers: ['spieder sense'],
+        id: "2",
+        name: "Spieder-Man",
+        superPowers: ["spieder sense"],
       },
       {
-        id: '3',
-        name: 'Batman',
-        superPowers: ['money', 'immortality'],
+        id: "3",
+        name: "Batman",
+        superPowers: ["money", "immortality"],
       },
       {
-        id: '4',
-        name: 'Ivan',
-        superPowers: ['Javascript'],
+        id: "4",
+        name: "Ivan",
+        superPowers: ["Javascript"],
       },
     ],
   },
   {
-    name: 'Dream Agent Team',
+    name: "Dream Agent Team",
     heroes: [
       {
-        id: '1',
-        name: 'James Bond',
-        superPowers: ['sexy', 'xharming', 'agility'],
+        id: "1",
+        name: "James Bond",
+        superPowers: ["sexy", "xharming", "agility"],
       },
       {
-        id: '2',
-        name: 'Json Bourne',
-        superPowers: ['losing memory'],
+        id: "2",
+        name: "Json Bourne",
+        superPowers: ["losing memory"],
       },
       {
-        id: '3',
-        name: 'Jack Bauer',
-        superPowers: ['punctuality'],
+        id: "3",
+        name: "Jack Bauer",
+        superPowers: ["punctuality"],
       },
     ],
   },
 ];
 
 // Función que itera sobre los poderes
-function* iteratePowers(superPowers) { 
-  for (let i = 0; i < superPowers.length; i++) { 
-    const superPower = superPowers[i]; 
-    yield superPower; 
+function* iteratePowers(superPowers, hero) {
+  // incluir una variable para el héroe
+  for (let i = 0; i < superPowers.length; i++) {
+    const superPower = superPowers[i];
+    yield { superPower, hero }; // retornar tanto el superpoder como el héroe
   }
 }
 
@@ -58,7 +59,7 @@ function* iteratePowers(superPowers) {
 function* iterateSuperHeores(superHeores) {
   for (let i = 0; i < superHeores.length; i++) {
     const superHeore = superHeores[i];
-    yield* iteratePowers(superHeore.superPowers);
+    yield* iteratePowers(superHeore.superPowers, superHeore); // enviar el héroe
   }
 }
 
@@ -73,24 +74,34 @@ function* iterateTeams(superHeoresTeams) {
 const generatorObject = iterateTeams(superHeroTeams);
 
 // Obteniendo el primer resultado
-let result = generatorObject.next();
+//let result = generatorObject.next();
 
 // Datos de busqueda
-const superPowerWanted = "immortality"
+const superPowerWanted = "immortality";
 let counter = 0;
 
-while (!result.done) {
-  const superPower = result.value;
+while (true) {
+  // cambiar a un bucle infinito para poder seguir iterando incluso después de encontrar el superpoder
+  const result = generatorObject.next();
+  if (result.done) {
+    break;
+  }
+
+  const { superPower, hero } = result.value; // desestructurar el valor de la generadora
   counter++;
   if (superPower === superPowerWanted) {
     // Solo se imprime que el super poder ha sido encontrado
     // pero no a que héroe pertenece
     // deberia imprimir algo asi
     // > El super poder de immortality le pertenece a Batman
-    console.log('Super Power has been found');
+    console.log(
+      `El super poder de ${superPowerWanted} le pertenece a ${hero.name}`
+    ); // imprimir el héroe asociado
     break;
-  } else {
+  } /*else {
     result = generatorObject.next();
-  }
+  }*/
 }
-console.log(`El sistema realizo ${counter} comparaciones en el conjunto de datos`);
+console.log(
+  `El sistema realizo ${counter} comparaciones en el conjunto de datos`
+);
